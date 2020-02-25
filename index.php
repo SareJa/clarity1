3e88f8d2e3a1161951dc00ce59b065145cd4db37
SareJa <sarajaneva7@gmail.com>
2020-02-25 22:29:41 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+       <meta charset="utf-8">
+	   <title></title>
+	   <meta name="author" content="">
+	   <meta name="viewport" content="width=device-width, initial-scale=2">
+	   <link href="../clarity/style.css" rel="stylesheet">
+    </head>
+    <body>
+
+        <h2>Get Fleet</h2>
+		
+		<form action="../clarity/getFleet.php" method="POST">
+			<input type="date" name="text">
+			<br><br>
+            <input type="submit" id="sendBtn" value="Send">
+		</form>
+<?php $today = date("F j, Y, g:i a"); echo $today;   ?>
+    </body>
+</html>
