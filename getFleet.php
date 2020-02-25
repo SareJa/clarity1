e4e9bab03ffc1dcc47b9d7a80b69b5b8a27d6e1d
SareJa <sarajaneva7@gmail.com>
2020-02-25 22:56:54 +0100

    Update getFleet.php

--- getFleet.php
+++ getFleet.php
@@ -45,7 +45,7 @@ if(mysqli_query($conn, $sql)){
 	
 	<table>
 		<tr>
-			<td>Timestamp Sara</td>
+			<td>Timestamp</td>
 			<td>Car Name</td>
 			<td>Engine coolant temperature</td>
 			<td>Engine RPM</td>
