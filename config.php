dc7102f6df5935e23c86db20a286f0d47a9a8a48
Sara <sara@Sara-Laptop>
2020-02-28 12:09:17 +0100

    Initial commit

--- config.php
+++ config.php
@@ -6,8 +6,10 @@ $pass = "toor1234!";
 $db = "clarity";
 $port = 3306;
 
-$conn = new mysqli($host, $user, $pass, $db, $port) or die("Connect failed: %s\n");
+$conn = mysqli_connect($host, $user, $pass, $db, $port) or die("Connect failed: %s\n");
 
-if($conn->connect_errno){
-    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+if($conn){
+    echo "Conected Successful <br>";
+} else {
+    echo "Not connected <br>";
 }
--- getFleet.php
+++ getFleet.php
@@ -2,6 +2,25 @@
 
 require_once 'config.php';
 
+//$conn = mysqli_connect($host, $user, $pass,$db) or die("Connect failed: %s\n");
+
+/* if($conn){
+echo "Conected Successful <br>";
+}
+else{
+echo "Not connected <br>";
+}
+
+$sql ="INSERT INTO test (day) VALUES('2')";
+
+if(mysqli_query($conn, $sql)){
+    echo "Records added successfully.";
+    echo "<br>";
+} else{
+    echo "Error: Could not able to execute $sql. " . mysqli_error($conn);
+}
+*/
+
 	
 	// Create a stream
 	$opts = array(
