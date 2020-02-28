a8f920d096c809a599887b483ba64c5281a9b586
Sara <sara@Sara-Laptop>
2020-02-28 09:28:35 +0100

    Initial commit

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$host = "localhost";
+$host = "127.0.0.1";
 $user = "root";
 $pass = "toor1234!";
 $db = "vehicles";
--- config_local.php
+++ config_local.php
@@ -1,6 +1,6 @@
 <?php
 
-$host = "127.0.0.1";
+$host = "localhost";
 $user = "phpmyadmin";
 $pass = "toor1234!";
 $db = "vehicles";
