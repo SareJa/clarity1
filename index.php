896cbc8b269c5851fac460b1a7d5fc611605deaf
Sara <sarajaneva7@gmail.com>
2020-09-23 22:29:09 +0200

    Add name test

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 	   <link href="../clarity/style.css" rel="stylesheet">
     </head>
     <body>
-
+	<h1>SARA JANEVAt</h1>
         <h2>Get Fleet</h2>
 		
 		<form action="../clarity/getFleet.php" method="POST">
